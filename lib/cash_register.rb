a94f1a6311f75e726b5db67fe6cbe058ff2f9530
Dan Foley <dan@dan-foley.com>
2020-02-03 02:43:15 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,11 @@
 class CashRegister
   
-  attr_accessor :empl_discount
+  attr_accessor :empl_discount, :total
   
   def initialize()
     @total = 0
   end
   
-  def discount(empl_discount)
-    
-  end
-  
-  
+
 
 end
