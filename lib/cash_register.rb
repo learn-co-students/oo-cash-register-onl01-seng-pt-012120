6f080bf319f29c783c1cf99330bf38ea6a200bcf
Karen Daniela <kdaniimora@gmail.com>
2020-02-17 19:04:43 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
-
+def CashRegister
+ attr_accessor :total, :apply_discount, :items, :void_last_transaction
+  
+  def initialize(discount = 0)
+    @total = 0 
+    @apply_discount = apply_discount
+    @item = []
+    end 
+  
+  def total
+    @total
+  end
+  
+  def items 
+    @items = []
+end 
+  
+  def void_last_transaction
+    
+end 
+  
+end 
+  
